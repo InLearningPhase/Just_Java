f3f5a975d820e0094bdf1e913856e5b5a07bdaa6
Tanesh Gupta <40913077+InLearningPhase@users.noreply.github.com>
2020-03-01 08:58:04 +0530

    Add a checkbox

--- app/src/main/java/com/example/android/justjava/MainActivity.java
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -59,8 +59,8 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void displayMessage(String message) {
-        TextView priceTextView = (TextView) findViewById(R.id.order_summary_text_view);
-        priceTextView.setText(message);
+        TextView orderSummaryTextView = (TextView) findViewById(R.id.order_summary_text_view);
+        orderSummaryTextView.setText(message);
     }
 
 }
